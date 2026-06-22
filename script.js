6fd25d46f50ae83452a5807f7dc6ced550a807ef
jsaayman-dotcom <joshsaayman14@gmail.com>
2026-06-22 16:23:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-alert("SCRIPT LOADED");
+emailjs.init({
+    publicKey: "vJkRPa-otxE7RW78s"
+});
 
 window.onload = function () {
 
@@ -9,11 +11,40 @@ window.onload = function () {
         return;
     }
 
-    alert("FORM FOUND");
+    form.addEventListener("submit", function (e) {
 
-    form.addEventListener("submit", function(e) {
         e.preventDefault();
-        alert("FORM SUBMITTED");
+
+        const templateParams = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            idnumber: document.getElementById("idnumber").value,
+            service: document.getElementById("service").value,
+            date: document.getElementById("date").value,
+            notes: document.getElementById("notes").value
+        };
+
+        emailjs.send(
+            "service_pyadlgy",
+            "template_vmkkrjg",
+            templateParams
+        )
+        .then(function () {
+
+            alert("Booking sent successfully!");
+
+            form.reset();
+
+        })
+        .catch(function (error) {
+
+            console.error("EmailJS Error:", error);
+
+            alert("Booking failed. Press F12 and check Console.");
+
+        });
+
     });
 
 };
