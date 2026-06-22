8b428d0543977e8ea1424ca16f76ffc8ac4f59a4
jsaayman-dotcom <joshsaayman14@gmail.com>
2026-06-22 16:05:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-emailjs.init({
-    publicKey: "vJkRPa-otxE7RW78s"
-});
+alert("SCRIPT LOADED");
 
 const form = document.getElementById("bookingForm");
 
-form.addEventListener("submit", function (e) {
+console.log(form);
 
-    e.preventDefault();
-
-    const templateParams = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        idnumber: document.getElementById("idnumber").value,
-        service: document.getElementById("service").value,
-        date: document.getElementById("date").value,
-        notes: document.getElementById("notes").value
-    };
-
-    emailjs.send(
-        "service_pyadlgy",
-        "template_vmkkrjg",
-        templateParams
-    )
-    .then(function(response) {
-        console.log("SUCCESS", response);
-        alert("Booking sent successfully!");
-    })
-    .catch(function(error) {
-        console.error("FAILED", error);
-        alert("Error: " + JSON.stringify(error));
+if(form){
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        alert("FORM WORKS");
     });
-
-});
+}
